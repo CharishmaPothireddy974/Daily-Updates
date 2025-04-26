2d61f1f4f31673ed76129878a4388d33b4a733e8
Charishma Pothireddy <charishmapothireddy974@gmail.com>
2025-04-26 11:52:34 +0530

    Create ChicagoPizzaStore.java

---
+++ April 26/Factory Method Pattern/ChicagoPizzaStore.java
@@ -0,0 +1,13 @@
+public class ChicagoPizzaStore extends PizzaStore{
+    protected Pizza createPizza(String type){
+        if (type.equals("cheese")){
+            return new ChicagoStyleCheesePizza();
+        }
+        else if (type.equals("veggie")){
+            return new ChicagoStyleVeggiePizza();
+        }
+        else{
+            return null;
+        }
+    }
+}
